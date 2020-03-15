57a6328c242d0b17dd9776a93f854eea81585c98
Arya S <arya.surya021@gmail.com>
2020-03-15 14:39:56 +0700

    Initial Commit

---
+++ Coronope/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Coronope
+//
+//  Created by IT Division on 15/03/20.
+//  Copyright © 2020 com.aryasurya. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Coronope/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Coronope
+//
+//  Created by IT Division on 15/03/20.
+//  Copyright © 2020 com.aryasurya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
